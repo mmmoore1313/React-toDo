6065c2fcf0ec6d918a88e2d32bdd2bca8d01d13e
Matt Moore <Matt.M.Moore13@outlook.com>
2021-11-02 18:50:04 -0400

    Began to shift toward component inheritance

--- src/App.js
+++ src/App.js
@@ -2,25 +2,7 @@ import React from "react"
 import './App.css';
 import { Button, Card, Form } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
-
-function Todo({ todo, index, markTodo, removeTodo }) {
-  return (
-    <div className="todo">
-      <span style={{ textDecoration: todo.isDone ? "line-through" : "" }}>
-        {todo.text}
-      </span>
-      <div>
-        <Button variant="outline-success" onClick={() => markTodo(index)}>
-          ✓
-        </Button>
-        {' '}
-        <Button variant="outline-danger" onClick={() => removeTodo(index)}>
-          ✕
-        </Button>
-      </div>
-    </div>
-  )
-}
+import Todo from './components/Todo/Todo.jsx'
 
 function FormTodo({ addTodo }) {
   const [value, setValue] = React.useState("")
